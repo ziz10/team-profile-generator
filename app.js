cfd38c609be6f1bac0c7bed104182957a668e1a4
Abdoul Bass <malickya8891@gmail.com>
2021-04-02 09:24:22 -0700

    created constansts

---
+++ app.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+const Manager = require("./lib/manager.js");
+let uniqueId = 0;
+let teamArray = [];
