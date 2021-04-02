90c377c374c9f9926f5bcfa553360345b3fcb7b8
Abdoul Bass <malickya8891@gmail.com>
2021-04-02 09:36:36 -0700

    created promptUser fonction and a engineerRes callback function

--- app.js
+++ app.js
@@ -5,3 +5,43 @@ const Intern = require("./lib/intern.js");
 const Manager = require("./lib/manager.js");
 let uniqueId = 0;
 let teamArray = [];
+
+
+
+function promptUser(answers) {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "what is your role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+    ]).then(function (res) {
+        
+        console.log(res)
+        if (res.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "github",
+                    type: "input",
+                    message: "What is your github Username?"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                }
+            ]).then(function (engineerRes) {
+                let newEngineer = new Engineer(engineerRes.name, engineerRes.email, uniqueId, engineerRes.github);
+                uniqueId = uniqueId + 1; // could be "uniqueId++"
+                console.log(newEngineer);
+                // run promptUser (called recursion) so that you can add multiple Engineers and id changes incrementally
+                teamArray.push(newEngineer);
+                addUser();
+                
+            });
