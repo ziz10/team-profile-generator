aeec30546fac7dfc30cb7721d21e181f78e618bf
Abdoul Bass <malickya8891@gmail.com>
2021-04-05 16:23:51 -0700

    implemented engeneer.test file

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,10 +1,19 @@
-const { test, expect } = require("@jest/globals")
-const Engineer=require("../Eneginerr")
-test("test to create Engineer", ()=>{
-    const engineer = new Engineer("John", 7, "johndoe@gmail.com")
-    expect(engineer.name).toBe("John")
-})
-test("getRole function",()=>{
-    const engineer = new Engineer("John", 7, "johndoe@gmail.com")
-    expect(engineer.getRole()).toBe("Engineer")
-})
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
+});
