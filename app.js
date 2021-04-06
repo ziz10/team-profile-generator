c9555a22d39ede8a0b7be87a22f6dbe3ff43747e
Abdoul Bass <malickya8891@gmail.com>
2021-04-06 12:43:11 -0700

    modified index.js

--- app.js
+++ app.js
@@ -1,105 +1,212 @@
 const inquirer = require("inquirer");
-const jest = require("jest");
-const Engineer = require("./lib/engineer.js");
-const Intern = require("./lib/intern.js");
-const Manager = require("./lib/manager.js");
-let uniqueId = 0;
-let teamArray = [];
+
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const fs = require("fs");
 
 
+function runInquirer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    }, {
+        type: "input",
+        message: "What is your ID?",
+        name: "id"
+    }, {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }, {
+        type: "list",
+        message: "What is your title",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title"
+    }];
 
-function promptUser(answers) {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "role",
-            message: "what is your role?",
-            choices: ["Engineer", "Intern", "Manager"]
-        },
-    ]).then(function (res) {
-        
-        console.log(res)
-        if (res.role === "Engineer") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is your name?",
-                    type: "input"
-                },
-                {
-                    name: "github",
-                    type: "input",
-                    message: "What is your github Username?"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is your email?"
-                }
-            ]).then(function (engineerRes) {
-                let newEngineer = new Engineer(engineerRes.name, engineerRes.email, uniqueId, engineerRes.github);
-                uniqueId = uniqueId + 1; // could be "uniqueId++"
-                console.log(newEngineer);
-                teamArray.push(newEngineer);
-                addUser();
-                
-            });
+    return inquirer
+        .prompt(promptArray);
+}
 
-        } else if (res.role === "Intern") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is your name?",
-                    type: "input"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is your email?"
-                },
-                {
-                    name: "school",
-                    type: "input",
-                    message: "Where did you graduate from college?"
-                }
-            ]).then(function (internRes) {
-                var newIntern = new Intern(internRes.name, internRes.email, uniqueId, internRes.school);
-                uniqueId = uniqueId + 1; // could be "uniqueId++"
-                console.log(newIntern)
-                teamArray.push(newIntern);
-                addUser();
-            });
+function runInquirerManager() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber"
+    }];
 
-        } else if (res.role === "Manager") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is your name?",
-                    type: "input"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is your email?"
-                },
-                {
-                    name: "office",
-                    type: "input",
-                    message: "What is your office number?"
-                }
-            ]).then(function (managerRes) {
-                var newManager = new Manager(managerRes.name, managerRes.email, uniqueId, managerRes.office);
-                uniqueId = uniqueId + 1; 
-                console.log(newManager);
-                teamArray.push(newManager);
-                addUser();
-            });
-        };
-       
+    return inquirer
+        .prompt(promptArray);
+}
 
-    })
-        .catch(function (err) {
-            console.log(err);
+function runInquirerEngineer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your github?",
+        name: "github"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+function runInquirerIntern() {
+    const promptArray = [{
+        type: "input",
+        message: "What school do you attend?",
+        name: "school"
+    }];
+
+    return inquirer
+        .prompt(promptArray);
+}
+
+
+async function run() {
+    let employeeArray = [];
+    const maxTimes = 4;
+    for (i = 0; i < maxTimes; i++) {
+        const promise = new Promise((resolve, reject) => {
+            runInquirer()
+                .then(function ({ name, id, email, title }) {
+
+                    if (title === "Manager") {
+                        runInquirerManager().then(function ({ officeNumber }) {
+                            this.employee = new Manager(name, id, email, officeNumber, title);
+                            console.log(officeNumber);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+
+                    } else if (title === "Engineer") {
+                        runInquirerEngineer().then(function ({ github }) {
+                            this.employee = new Engineer(name, id, email, github, title);
+                            console.log(github);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+                    } else if (title === "Intern") {
+                        runInquirerIntern().then(function ({ school }) {
+                            this.employee = new Intern(name, id, email, school, title);
+                            console.log(school);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+                    }
+
+                }).catch(function (err) {
+                    console.log("Error!!!!!.");
+                    console.log(err);
+                });
         });
 
-};
+        const result = await promise;
+        console.log(result);
+    }
+
+  
+
+    function displayTitle(employee) {
+        if (employee.title === "Manager") {
+            console.log(employee.officeNumber);
+            return `office number: ${employee.officeNumber}`;
+        }
+
+        if (employee.title === "Intern") {
+            return `school: ${employee.school}`;
+        }
+
+        if (employee.title === "Engineer") {
+            return `gitHub: ${employee.github}`;
+        }
+
+    }
+    function getCardHtml() {
+        let html = "";
+        for (j = 0; j < maxTimes; j++) {
+            console.log(employeeArray[j])
+            html += `<div class="card bg-dark justify-content-center align-items-center" style="width: 18rem;">
+                <div class="col card-header">
+                    <h4>${employeeArray[j].name}</h4>
+                </div>
+                <div class="col card-header">
+                    <h4>${employeeArray[j].title}</h4 >
+                </div >
+                <ul class="list-group list-group-flush text">
+                    <li class="list-group-item">ID: ${employeeArray[j].id}</li>
+                    <li class="list-group-item">Email: ${employeeArray[j].email}</li>
+                    <li class="list-group-item"> ${displayTitle(employeeArray[j])}</li>
+                </ul>
+            </div > `;
+        }
+        return html;
+    }
+
+
+
+    let html = `< !DOCTYPE html >
+                <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                                        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                                        <title>Document</title>
+                                        <style>
+                                            .row {
+                                                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+            .card {
+                                                padding: 15px;
+                border-radius: 5px;
+                background-color: white;
+                color: lightskyblue;
+                margin: 15px;
+            }
+            .text {
+                                                padding: 15px;
+                border-radius: 5px;
+                background-color: lightskyblue;
+                color: black;
+                margin: 15px;
+            }
+            .col {
+                                                flex: 1;
+                text-align: center;
+            }
+        </style>
+    </head>
+                                    <body>
+                                        <nav class="navbar navbar-dark bg-dark justify-content-center align-items-center">
+                                            <span class="navbar-brand mb-0 h1">
+                                                <h1>My Team</h1>
+                                            </span>
+                                        </nav>
+                                        <div class="row">
+                                            ${getCardHtml()}
+                                        </div>
+                                    </body>
+    
+    </html>
+    `;
+
+
+
+
+    console.log(html);
+    const fs = require("fs");
+    fs.writeFile('newfile.html', html, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+}
+run()
